1c142c28b26e75f240c905637cfc455a5ed6e342
Victor Gago do Nascimento <victor.nascimento@venturus.org.br>
2019-03-18 09:13:36 -0300

    Added a option to mongoose connection

--- config/database.js
+++ config/database.js
@@ -2,12 +2,17 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 try {
-    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+    mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    });
 } catch (error) {
     mongoose.createConnection(process.env.MONGODB_URI);
 }
 
 mongoose.connection.once('open', _ => console.log('MongoDB Running'))
-    .on('error', e => { throw e; });
+    .on('error', e => {
+        throw e;
+    });
 
 exports.mongoose
